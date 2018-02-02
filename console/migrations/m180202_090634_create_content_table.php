778a94489fc20ea3c678edee63853f04edc70da8
fazanis31 <fazanis31@mail.ru>
2018-02-02 15:21:23 +0600

    Добавлена миграция для таблицы content

---
+++ console/migrations/m180202_090634_create_content_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `content`.
+ */
+class m180202_090634_create_content_table extends Migration
+{
+    /**
+     * @inheritdoc
+     */
+    public function up()
+    {
+        $this->createTable('content', [
+            'id' => $this->primaryKey(),
+            'cat' => $this->integer()->notNull(),
+            'name_ru' => $this->string(255),
+            'content_ru'=>$this->text(),
+            'name_kz' => $this->string(255),
+            'content_kz'=>$this->text(),
+            'foto'=>$this->string(255);
+
+
+        ]);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function down()
+    {
+        $this->dropTable('content');
+    }
+}
