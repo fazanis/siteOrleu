ec912f14b2b17034f9b86a492ca899f68cde5ec7
fazanis <fazanis@mail.ru>
2018-02-24 23:39:06 +0600

    Merge branch 'works'

--- frontend/module/admin/views/catnews/cattree.php
+++ frontend/module/admin/views/catnews/cattree.php
@@ -8,7 +8,7 @@
     <li><?=Html::a($parentcat->name_ru,['/admin/catnews/update','id'=>$parentcat->id],['target'=>'_blanck'])?></li>
     <ul>
         <? foreach ($parentcat->ChildCat($parentcat->id) as $item):?>
-        <li><?=Html::a($item->name_ru,['/admin/catnews/update','id'=>$item->id],['target'=>'_blanck'])?></li>
+        <li><?=Html::a($item ->name_ru,['/admin/catnews/update','id'=>$item->id],['target'=>'_blanck'])?></li>
         <?endforeach;?>
     </ul>
 </ul>
