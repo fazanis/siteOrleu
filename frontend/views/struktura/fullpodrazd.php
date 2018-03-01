080e9085341a1a362064c89271ab89307cd85588
fazanis31 <fazanis31@mail.ru>
2018-03-01 17:34:43 +0600

    Раздел руководство, кафедры и отделы

--- frontend/views/struktura/fullpodrazd.php
+++ frontend/views/struktura/fullpodrazd.php
@@ -4,19 +4,34 @@ use yii\helpers\Html;
 ?>
     <h1><?=$otdelinfo->{'name_'.WLang::getLang()}?></h1>
 <?=$otdelinfo->{'text_'.WLang::getLang()}?>
-
+<hr>
 <?foreach ($ankety as $anketa):?>
 
 
     <?
     $img = $anketa->getImage();
     ?>
-    <?=Html::img($img->getUrl(),['alt'=>$item->fio_ru]) ?>
-    <h3><?=$anketa->{'dolzhnost_'.\frontend\widgets\WLang::getLang()}?></h3>
-    <h1><?=$anketa->{'fio_'.\frontend\widgets\WLang::getLang()}?></h1>
-    <p><?=$anketa->{'publikacii_'.\frontend\widgets\WLang::getLang()}?></p>
-    <p><b>Телефон:</b> <?=$anketa->phone?></p>
-    <p><b>Email:</b> <?=$anketa->email?></p>
-    <p><b>Кабинет:</b> <?=$anketa->kabinet?></p>
+
+    <div class="job-media row">
+        <div class="col-sm-5">
+            <?=Html::img($img->getUrl(),['alt'=>$item->fio_ru,'class'=>'img-responsive']) ?>
+        </div>
+
+        <div class="col-sm-7 job-details">
+            <h3><?=$anketa->{'fio_'.WLang::getLang()}?></h3>
+            <ul class="">
+                <li><strong><i class="fa fa-user"></i> Должность :</strong> <?=$anketa->{'dolzhnost_'.WLang::getLang()}?></li>
+                <li><strong><i class="fa fa-briefcase"></i> Телефон :</strong> <?=$anketa->phone?></li>
+                <li><strong><i class="fa fa-dollar"></i> Email :</strong> <?=$anketa->email?></li>
+                <li><strong><i class="fa fa-clock-o"></i> Кабинет №:</strong> <?=$anketa->kabinet?></li>
+            </ul>
+        </div>
+    </div>
+
+    <h3><?=Yii::t('app','Награды')?></h3>
+    <p><?=$anketa->{'nagradi_'.WLang::getLang()}?></p>
+    <h3><?=Yii::t('app','Публикации')?></h3>
+    <p><?=$anketa->{'publikacii_'.WLang::getLang()}?></p>
+    <hr>
 <?endforeach;?>
 
