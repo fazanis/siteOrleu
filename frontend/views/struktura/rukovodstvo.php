1a5e25a7e9d63c466f9230d4c3cdc57347157eaa
fazanis31 <fazanis31@mail.ru>
2018-06-01 11:52:10 +0600

    fixet page rukovodstvo

--- frontend/views/struktura/rukovodstvo.php
+++ frontend/views/struktura/rukovodstvo.php
@@ -1,18 +1,21 @@
 <?php
 /* @var $this yii\web\View */
+
 use yii\helpers\Html;
+
 ?>
-<?$this->title = 'Руководство';?>
-<?foreach ($model as $item):?>
+<? $this->title = 'Руководство'; ?>
+<? foreach ($model as $item): ?>
 
     <?
-        $img = $item->getImage();
+    $img = $item->getImage();
     ?>
-    <?=Html::img($img->getUrl(),['alt'=>$item->fio_ru]) ?>
-    <h3><?=$item->{'dolzhnost_'.\frontend\widgets\WLang::getLang()}?></h3>
-    <h1><?=$item->{'fio_'.\frontend\widgets\WLang::getLang()}?></h1>
-    <p><?=$item->{'publikacii_'.\frontend\widgets\WLang::getLang()}?></p>
-    <p><b>Телефон:</b> <?=$item->phone?></p>
-    <p><b>Email:</b> <?=$item->email?></p>
-    <p><b>Кабинет:</b> <?=$item->kabinet?></p>
-<?endforeach;?>
+    <center><?= Html::img($img->getUrl(), ['alt' => $item->fio_ru]) ?>
+        <h3><?= $item->{'dolzhnost_' . \frontend\widgets\WLang::getLang()} ?></h3>
+        <h1><?= $item->{'fio_' . \frontend\widgets\WLang::getLang()} ?></h1>
+    </center>
+    <p><?= $item->{'publikacii_' . \frontend\widgets\WLang::getLang()} ?></p>
+    <p><b>Телефон:</b> <?= $item->phone ?></p>
+    <p><b>Email:</b> <?= $item->email ?></p>
+    <p><b>Кабинет:</b> <?= $item->kabinet ?></p>
+<? endforeach; ?>
